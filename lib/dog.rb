d35252d95e2dadea8e484a384d6278f215e849cb
Domenico Cascone <dcascone84@gmail.com>
2020-03-16 12:56:21 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,12 @@
 class Dog
-  def name=(dog_name)
-  @the_dog_name = dog_name
+#  def name=(dog_name)
+#  @the_dog_name = dog_name
+#  end
+  def initialize(name)
+    @name = name
+  end
+
+  def name
+
   end
 end
